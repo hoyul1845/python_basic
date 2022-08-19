313b69c34816f40ea5293fce55e158ae40c57f0c
hoyul1845 <hoyul1845@naver.com>
2022-08-19 21:05:30 +0900

    간단한 명령어 추가

--- 4.5.practice.py
+++ 4.5.practice.py
@@ -1,1 +1,26 @@
-print("백문이 불여의견 백견이 불여일타")
+print("백문이 불여의견\n백견이 불여일타")
+
+# 저는 "나도코딩"입니다
+print('저는 "나도코딩"입니다.')
+print("저는 \"나도코딩\"입니다")
+print("저는 \'나도코딩\'입니다")
+
+
+
+# \\ : 문장내에서 \
+
+
+
+
+
+print("Red Apple\rPine")
+
+
+
+
+
+print("Redd\bApple")
+
+
+#\t : 탭
+print("Red\tApple")
