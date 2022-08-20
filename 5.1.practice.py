610134cf6019c1dc25a01bff2fe0f274276f183a
hoyul1845 <hoyul1845@naver.com>
2022-08-20 10:18:24 +0900

    5.1.practice 파일 추가 + 간단한 코드 추가

--- 5.1.practice.py
+++ 5.1.practice.py
@@ -19,4 +19,45 @@ print(subway)
     
     
 # 정형돈씨를 유재석 / 조세호 사이에 태워봄
-subway.insert("정형돈", 1)
+subway.insert(1, "정형돈")
+print(subway)
+
+
+print(subway.pop())
+print(subway)
+
+'''print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)'''
+
+
+subway.append("유재석")
+print(subway)
+print(subway.count("유재석"))
+
+
+num_list = [1,0,0,4,0]
+num_list.sort()
+print(num_list)
+
+
+num_list.reverse()
+print(num_list)
+
+# 모두 지우기 
+#num_list.clear()
+#print(num_list)
+
+# 다양한 자료형 함께사용
+num_list = [1,0,0,4,0]
+mix_list = ["조세호", 20, True]
+#print(mix_list)
+
+#리스트 확장
+num_list.extend(mix_list)
+print(num_list)
+
+
+
