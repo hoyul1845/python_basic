82b38bac30693e721a7daa6de1fff486374bb56a
hoyul1845 <hoyul1845@naver.com>
2022-08-21 21:10:33 +0900

    7.2.전달값과반환값

--- 7.2.practice.py
+++ 7.2.practice.py
@@ -1,27 +1,27 @@
 def open_account():
     print("새로운 계좌가 생성되었습니다.")
     
-open_account()
 
-def deposit(balance, money):
+def deposit(balance, money): # 입금
     print("입금이 완료되었습니다. 잔액은{0} 원 입니다".format(balance + money))
     return balance + money
 
-def withdraw(balance, money): 
-    if balance >= money:
+def withdraw(balance, money): # 출금
+    if balance >= money: # 잔액에 출금보다 많으면
         print("출금이 완료 되었습니다. 잔액은 {0} 원입니다.".format(balance - money))
         return balance - money
     else:
         print("출금이 완료되지 않았습니다. 잔액은 {0} 원 입니다.".format(balance))
         return balance
     
-def withdraw_night(balance, money):
-    commisson = 100
+def withdraw_night(balance, money): # 저녘에 출금
+    commisson = 100 # 수수료 100만원
     return commisson, balance - money - commisson
     
 
-balance = 0
+balance = 0 # 잔액
 balance = deposit(balance, 1000)
+#balance = withdraw(balance, 2000)
 #balance = withdraw(balance, 500)
 commisson, balance = withdraw_night(balance, 500)
 print("수수료는 {0} 원이며, 잔액은 {1} 원입니다.".format(commisson, balance))
