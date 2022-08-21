6e97006c296a9d1eedaf0d934848a56af2691925
hoyul1845 <hoyul1845@naver.com>
2022-08-21 20:02:37 +0900

    지역변수와 전역변수

---
+++ 7.6.practice.py
@@ -0,0 +1,16 @@
+gun = 10
+
+def checkpoint(soldiers): # 경계근무
+    global gun
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    
+def checkpoint_ret(gun, soldiers):
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    return gun
+    
+print("전체 총 : {0}".format(gun))
+#checkpoint(2)
+gun = checkpoint_ret(gun, 2)
+print("남은 총 : {0}".format(gun))
