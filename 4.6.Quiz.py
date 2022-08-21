037a814554e3f2c9ae87c2b255735c7f310e2fdf
hoyul1845 <hoyul1845@naver.com>
2022-08-21 20:36:38 +0900

    퀴즈 #3

--- 4.6.Quiz.py
+++ 4.6.Quiz.py
@@ -9,7 +9,8 @@
 url = "http://naver.com"
 my_str = url.replace("http://", "") # 규칙 1
 # print(my_str)
-my_str = my_str[:my_str.index(".")] #my_str[0:5]
-print(my_str)
+my_str = my_str[:my_str.index(".")] # 규칙 2
+# my_str[0:5] -> 0 ~ 5 직전까지. [0, 1, 2, 3, 4]
+#print(my_str)
 password = my_str[:3] + str(len(my_str)) + str(my_str.count("e"))
 print(" {0} 의 비밀번호는 {1} 입니다.".format(url, password))
