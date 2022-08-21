74c93d95d32e169b83a4560fd6fb67d8f940eb88
hoyul1845 <hoyul1845@naver.com>
2022-08-21 20:50:05 +0900

    5.4.세트

--- 5.4.practice.py
+++ 5.4.practice.py
@@ -11,18 +11,19 @@ python = set(["유재석", "박명수"])
 print(java & python)
 print(java.intersection(python))
 
-
+# 한집합 (java 할 수있거나 python 할 수 있는 개발자)
 print(java | python)
 print(java.union(python))
 
+# 차집합 (java 할 수 있지만 python 은 할 줄 모르는 개발자)
 print(java - python)
 print(java.difference(python))
 
 
-
+# python 할 줄 아는 사람이 늘어남
 python.add("김태호")
 print(python)
 
-
+# java 를 잊었어요
 java.remove("김태호")
 print(java)
