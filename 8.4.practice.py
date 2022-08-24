835a59a82244fd3d1a27210bd5348e96f31ff9b0
hoyul1845 <hoyul1845@naver.com>
2022-08-24 21:49:43 +0900

    8.4.pickle

--- 8.4.practice.py
+++ 8.4.practice.py
@@ -9,4 +9,4 @@ import pickle
 profile_file = open("profile.pickle", "rb")
 profile = pickle.load(profile_file) # file 에 있는 정보를 profile 에 불러오기
 print(profile)
-profile_file.close 
+profile_file.close      
