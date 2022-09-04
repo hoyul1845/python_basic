e878727edb1321066cb7760e7cdd8f06fdc4efe8
hoyul1845 <hoyul1845@naver.com>
2022-09-04 19:57:11 +0900

    11.4.모듈 직접 실행

---
+++ 11.4.practice.py
@@ -0,0 +1,19 @@
+# import travel.tailand
+# import travel.tailand.ThailandPackage
+# trip_to = travel.tailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel.tailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+#from random import *
+from travel import *
+# trip_to = vietnam.VietnamPackage()
+trip_to = tailand.ThailandPackage()
+trip_to.detail()
+
